a2a4e85db721c9a256834c92e3280476cc1c5ee3
Siddheshs07 <siddeshsalimath07@gmail.com>
2024-08-19 16:51:11 +0530

    pagination excluded

--- controllers/places.js
+++ controllers/places.js
@@ -58,11 +58,11 @@ const getAllPlacesTesting = async (req, res) => {
     let selectFix = select.split(",").join(" ");
     apiData = apiData.select(selectFix);
   }
-  let page = Number(req.query.page) || 1;
-  let limit = Number(req.query.limit) || 6;
-  let skip = (page - 1) * limit;
+  // let page = Number(req.query.page) || 1;
+  // let limit = Number(req.query.limit) || 6;
+  // let skip = (page - 1) * limit;
 
-  apiData = apiData.skip(skip).limit(limit);
+  // apiData = apiData.skip(skip).limit(limit);
 
   const Places = await apiData;
   res.status(200).json({ Places, nbHits: Places.length });
