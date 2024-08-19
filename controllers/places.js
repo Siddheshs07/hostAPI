be7e9f17a204ea023d02e38ae0c6c0782a803f6a
Siddheshs07 <siddeshsalimath07@gmail.com>
2024-08-19 22:46:32 +0530

    fixed filter by category_slugs

--- controllers/places.js
+++ controllers/places.js
@@ -33,25 +33,26 @@ const getAllPlaces = async (req, res) => {
     apiData = apiData.select(selectFix);
   }
 
-  // let page = Number(req.query.page) || 1;
-  // let limit = Number(req.query.limit) || 6;
-
-  // let skip = (page - 1) * limit;
-
-  // apiData = apiData.skip(skip).limit(limit);
-
   const Places = await apiData;
   res.status(200).json({ Places, nbHits: Places.length });
 };
 
 const getAllPlacesTesting = async (req, res) => {
-  const { category_slugs, name, sort, select } = req.query;
+  const { category_slugs, name, sort, select, _id } = req.query;
 
   const queryObject = {};
+  const paramsArray = Array.isArray(category_slugs)
+    ? category_slugs
+    : category_slugs
+    ? category_slugs.split(",")
+    : [];
 
   if (category_slugs) {
-    queryObject.category_slugs = { $regex: category_slugs, $options: "i" };
+    queryObject.category_slugs = { $in: paramsArray };
     console.log(queryObject.category_slugs);
+  }
+  if (_id) {
+    queryObject._id = _id;
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
@@ -59,15 +60,19 @@ const getAllPlacesTesting = async (req, res) => {
   let apiData = PlacesSchema.find(queryObject);
 
   if (sort) {
+    // let sortFix = sort.replace(",", " ");
     let sortFix = sort.split(",").join(" ");
     apiData = apiData.sort(sortFix);
   }
   if (select) {
+    // let selectFix = select.replace(",", " ");
     let selectFix = select.split(",").join(" ");
     apiData = apiData.select(selectFix);
   }
+
   // let page = Number(req.query.page) || 1;
   // let limit = Number(req.query.limit) || 6;
+
   // let skip = (page - 1) * limit;
 
   // apiData = apiData.skip(skip).limit(limit);
